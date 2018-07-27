acb5f2485bc0dba3c23c448019a6a2f6400f1aba
Andrew VanVlack <A.VanVlack@Gmail.com>
2018-07-27 02:46:10 -0700

    added light and spawn command

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const morgan = require("morgan")
 const acIR = require("./utils/acIR.js")
+const { spawn } = require('child_process');
 
 const app = express()
 app.use(morgan('dev'))
@@ -12,7 +13,20 @@ app.post('/updateState', (req, res) => {
   let data = req.body
   if (!data.power && data.state === "off") data.state = 'fan', data.power = 'off'
   else data.power = 'on'
+
   let irCode = acIR.buildIR(data)
+
+  const light = spawn('./light', [irCode.toString()])
+  light.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+  light.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+  });
+  light.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+
   res.send(irCode)
 })
 
